21a17fb2ffbbf0c9e1c1187b9b0501a3cfbd02b1
sacsbrainz <solomonogu0@gmail.com>
2022-07-01 22:51:22 +0100

    second push

--- pages/Homepage.tsx
+++ pages/Homepage.tsx
@@ -36,7 +36,7 @@ function Homepage() {
           </div>
           <div>
             <div className='flex flex-row gap-4 '>
-              <a href=''>
+              <a href='https://www.instagram.com/jhainellesbeautypalace'>
                 <div className=' rounded-full bg-slate-700 p-2'>
                   <svg
                     xmlns='http://www.w3.org/2000/svg'
@@ -48,7 +48,7 @@ function Homepage() {
                   </svg>
                 </div>
               </a>
-              <a href=''>
+              {/* <a href=''>
                 <div className='rounded-full bg-slate-700 p-2'>
                   <svg
                     xmlns='http://www.w3.org/2000/svg'
@@ -59,7 +59,7 @@ function Homepage() {
                     <path d='M13 9h4.5l-.5 2h-4v9h-2v-9H7V9h4V7.128c0-1.783.186-2.43.534-3.082a3.635 3.635 0 0 1 1.512-1.512C13.698 2.186 14.345 2 16.128 2c.522 0 .98.05 1.372.15V4h-1.372c-1.324 0-1.727.078-2.138.298-.304.162-.53.388-.692.692-.22.411-.298.814-.298 2.138V9z' />
                   </svg>
                 </div>
-              </a>
+              </a> */}
               <a href='tel:08134765916'>
                 <div className=' rounded-full bg-slate-700 p-2'>
                   <svg
